b75682fed71dff0bde2f0e51aed78498c841a407
jl8629 <jl8629@srmist.edu.in>
2026-03-06 08:56:07 +0530

    Jasmitha added UC1-WelcomePage

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,7 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        String appName = "Hotel Booking System";
+        String version = "v1.0";
+        System.out.println("Welcome to " + appName + " " + version);
+    }
+}
